9cb3321767c3df7005e04acfebacb399b2adaa61
julius75 <juliusnchagwa75@gmail.com>
2021-01-29 16:49:23 +0300

    initials name

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -1363,12 +1363,16 @@
             </div>
             <!--end::Languages-->
             <!--begin::User-->
+            <?php
+            use Illuminate\Support\Facades\Auth;
+            $user = Auth::guard('admin')->user();
+            ?>
             <div class="topbar-item">
                 <div class="btn btn-icon btn-icon-mobile w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
                     <span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hi,</span>
                     <span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3"></span>
                     <span class="symbol symbol-lg-35 symbol-25 symbol-light-success">
-                        <span class="symbol-label font-size-h5 font-weight-bold"></span>
+                        <span class="symbol-label font-size-h5 font-weight-bold">{{ucfirst(substr($user->username, 0, 1))}}</span>
                     </span>
                 </div>
             </div>
