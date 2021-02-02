a9246cf721e560a362c4e514cdf330c9c929115f
Kevin Mukhami <mukhami@deveint.com>
2021-02-02 12:54:44 +0300

    updated docs, laravel passport fix

--- resources/views/scribe/index.blade.php
+++ resources/views/scribe/index.blade.php
@@ -520,6 +520,357 @@ Password.</p>
 <br>
 Password, must match password.</p>
 
+</form><h1>Uer Profile Management</h1>
+<p>APIs for managing user profile and phone numbers</p>
+<h2>User&#039;s Profile</h2>
+<p><small class="badge badge-darkred">requires authentication</small></p>
+<p>Provides data for the profile page e.g. first_name, last_name, email and phone numbers</p>
+<blockquote>
+<p>Example request:</p>
+</blockquote>
+<pre><code class="language-bash">curl -X POST \
+    "http://localhost/api/v1/user/profile" \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json"</code></pre>
+<pre><code class="language-javascript">const url = new URL(
+    "http://localhost/api/v1/user/profile"
+);
+
+let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+};
+
+fetch(url, {
+    method: "POST",
+    headers,
+}).then(response =&gt; response.json());</code></pre>
+<div id="execution-results-POSTapi-v1-user-profile" hidden>
+    <blockquote>Received response<span id="execution-response-status-POSTapi-v1-user-profile"></span>:</blockquote>
+    <pre class="json"><code id="execution-response-content-POSTapi-v1-user-profile"></code></pre>
+</div>
+<div id="execution-error-POSTapi-v1-user-profile" hidden>
+    <blockquote>Request failed with error:</blockquote>
+    <pre><code id="execution-error-message-POSTapi-v1-user-profile"></code></pre>
+</div>
+<form id="form-POSTapi-v1-user-profile" data-method="POST" data-path="api/v1/user/profile" data-authed="1" data-hasfiles="0" data-headers='{"Content-Type":"application\/json","Accept":"application\/json"}' onsubmit="event.preventDefault(); executeTryOut('POSTapi-v1-user-profile', this);">
+<h3>
+    Request&nbsp;&nbsp;&nbsp;
+        <button type="button" style="background-color: #8fbcd4; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-tryout-POSTapi-v1-user-profile" onclick="tryItOut('POSTapi-v1-user-profile');">Try it out ⚡</button>
+    <button type="button" style="background-color: #c97a7e; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-canceltryout-POSTapi-v1-user-profile" onclick="cancelTryOut('POSTapi-v1-user-profile');" hidden>Cancel</button>&nbsp;&nbsp;
+    <button type="submit" style="background-color: #6ac174; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-executetryout-POSTapi-v1-user-profile" hidden>Send Request 💥</button>
+    </h3>
+<p>
+<small class="badge badge-black">POST</small>
+ <b><code>api/v1/user/profile</code></b>
+</p>
+<p>
+<label id="auth-POSTapi-v1-user-profile" hidden>Authorization header: <b><code>Bearer </code></b><input type="text" name="Authorization" data-prefix="Bearer " data-endpoint="POSTapi-v1-user-profile" data-component="header"></label>
+</p>
+</form>
+<h2>Edit User&#039;s Profile</h2>
+<p><small class="badge badge-darkred">requires authentication</small></p>
+<p>Change User Profile details</p>
+<blockquote>
+<p>Example request:</p>
+</blockquote>
+<pre><code class="language-bash">curl -X POST \
+    "http://localhost/api/v1/user/profile/edit" \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -d '{"first_name":"accusantium","last_name":"pariatur","email":"minima"}'
+</code></pre>
+<pre><code class="language-javascript">const url = new URL(
+    "http://localhost/api/v1/user/profile/edit"
+);
+
+let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+};
+
+let body = {
+    "first_name": "accusantium",
+    "last_name": "pariatur",
+    "email": "minima"
+}
+
+fetch(url, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(body),
+}).then(response =&gt; response.json());</code></pre>
+<div id="execution-results-POSTapi-v1-user-profile-edit" hidden>
+    <blockquote>Received response<span id="execution-response-status-POSTapi-v1-user-profile-edit"></span>:</blockquote>
+    <pre class="json"><code id="execution-response-content-POSTapi-v1-user-profile-edit"></code></pre>
+</div>
+<div id="execution-error-POSTapi-v1-user-profile-edit" hidden>
+    <blockquote>Request failed with error:</blockquote>
+    <pre><code id="execution-error-message-POSTapi-v1-user-profile-edit"></code></pre>
+</div>
+<form id="form-POSTapi-v1-user-profile-edit" data-method="POST" data-path="api/v1/user/profile/edit" data-authed="1" data-hasfiles="0" data-headers='{"Content-Type":"application\/json","Accept":"application\/json"}' onsubmit="event.preventDefault(); executeTryOut('POSTapi-v1-user-profile-edit', this);">
+<h3>
+    Request&nbsp;&nbsp;&nbsp;
+        <button type="button" style="background-color: #8fbcd4; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-tryout-POSTapi-v1-user-profile-edit" onclick="tryItOut('POSTapi-v1-user-profile-edit');">Try it out ⚡</button>
+    <button type="button" style="background-color: #c97a7e; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-canceltryout-POSTapi-v1-user-profile-edit" onclick="cancelTryOut('POSTapi-v1-user-profile-edit');" hidden>Cancel</button>&nbsp;&nbsp;
+    <button type="submit" style="background-color: #6ac174; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-executetryout-POSTapi-v1-user-profile-edit" hidden>Send Request 💥</button>
+    </h3>
+<p>
+<small class="badge badge-black">POST</small>
+ <b><code>api/v1/user/profile/edit</code></b>
+</p>
+<p>
+<label id="auth-POSTapi-v1-user-profile-edit" hidden>Authorization header: <b><code>Bearer </code></b><input type="text" name="Authorization" data-prefix="Bearer " data-endpoint="POSTapi-v1-user-profile-edit" data-component="header"></label>
+</p>
+<h4 class="fancy-heading-panel"><b>Body Parameters</b></h4>
+<p>
+<b><code>first_name</code></b>&nbsp;&nbsp;<small>string</small>  &nbsp;
+<input type="text" name="first_name" data-endpoint="POSTapi-v1-user-profile-edit" data-component="body" required  hidden>
+<br>
+Last Name.</p>
+<p>
+<b><code>last_name</code></b>&nbsp;&nbsp;<small>string</small>  &nbsp;
+<input type="text" name="last_name" data-endpoint="POSTapi-v1-user-profile-edit" data-component="body" required  hidden>
+<br>
+First Name.</p>
+<p>
+<b><code>email</code></b>&nbsp;&nbsp;<small>string</small>  &nbsp;
+<input type="text" name="email" data-endpoint="POSTapi-v1-user-profile-edit" data-component="body" required  hidden>
+<br>
+Email Address.</p>
+
+</form>
+<h2>Delete Account</h2>
+<p><small class="badge badge-darkred">requires authentication</small></p>
+<p>Deactivates user account</p>
+<blockquote>
+<p>Example request:</p>
+</blockquote>
+<pre><code class="language-bash">curl -X POST \
+    "http://localhost/api/v1/user/profile/delete-account" \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json"</code></pre>
+<pre><code class="language-javascript">const url = new URL(
+    "http://localhost/api/v1/user/profile/delete-account"
+);
+
+let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+};
+
+fetch(url, {
+    method: "POST",
+    headers,
+}).then(response =&gt; response.json());</code></pre>
+<div id="execution-results-POSTapi-v1-user-profile-delete-account" hidden>
+    <blockquote>Received response<span id="execution-response-status-POSTapi-v1-user-profile-delete-account"></span>:</blockquote>
+    <pre class="json"><code id="execution-response-content-POSTapi-v1-user-profile-delete-account"></code></pre>
+</div>
+<div id="execution-error-POSTapi-v1-user-profile-delete-account" hidden>
+    <blockquote>Request failed with error:</blockquote>
+    <pre><code id="execution-error-message-POSTapi-v1-user-profile-delete-account"></code></pre>
+</div>
+<form id="form-POSTapi-v1-user-profile-delete-account" data-method="POST" data-path="api/v1/user/profile/delete-account" data-authed="1" data-hasfiles="0" data-headers='{"Content-Type":"application\/json","Accept":"application\/json"}' onsubmit="event.preventDefault(); executeTryOut('POSTapi-v1-user-profile-delete-account', this);">
+<h3>
+    Request&nbsp;&nbsp;&nbsp;
+        <button type="button" style="background-color: #8fbcd4; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-tryout-POSTapi-v1-user-profile-delete-account" onclick="tryItOut('POSTapi-v1-user-profile-delete-account');">Try it out ⚡</button>
+    <button type="button" style="background-color: #c97a7e; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-canceltryout-POSTapi-v1-user-profile-delete-account" onclick="cancelTryOut('POSTapi-v1-user-profile-delete-account');" hidden>Cancel</button>&nbsp;&nbsp;
+    <button type="submit" style="background-color: #6ac174; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-executetryout-POSTapi-v1-user-profile-delete-account" hidden>Send Request 💥</button>
+    </h3>
+<p>
+<small class="badge badge-black">POST</small>
+ <b><code>api/v1/user/profile/delete-account</code></b>
+</p>
+<p>
+<label id="auth-POSTapi-v1-user-profile-delete-account" hidden>Authorization header: <b><code>Bearer </code></b><input type="text" name="Authorization" data-prefix="Bearer " data-endpoint="POSTapi-v1-user-profile-delete-account" data-component="header"></label>
+</p>
+</form>
+<h2>Add Phone Number</h2>
+<p><small class="badge badge-darkred">requires authentication</small></p>
+<p>Adds additional Phone Number to a users profile</p>
+<blockquote>
+<p>Example request:</p>
+</blockquote>
+<pre><code class="language-bash">curl -X POST \
+    "http://localhost/api/v1/user/profile/add-phone_number" \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -d '{"phone_number":"quibusdam"}'
+</code></pre>
+<pre><code class="language-javascript">const url = new URL(
+    "http://localhost/api/v1/user/profile/add-phone_number"
+);
+
+let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+};
+
+let body = {
+    "phone_number": "quibusdam"
+}
+
+fetch(url, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(body),
+}).then(response =&gt; response.json());</code></pre>
+<div id="execution-results-POSTapi-v1-user-profile-add-phone_number" hidden>
+    <blockquote>Received response<span id="execution-response-status-POSTapi-v1-user-profile-add-phone_number"></span>:</blockquote>
+    <pre class="json"><code id="execution-response-content-POSTapi-v1-user-profile-add-phone_number"></code></pre>
+</div>
+<div id="execution-error-POSTapi-v1-user-profile-add-phone_number" hidden>
+    <blockquote>Request failed with error:</blockquote>
+    <pre><code id="execution-error-message-POSTapi-v1-user-profile-add-phone_number"></code></pre>
+</div>
+<form id="form-POSTapi-v1-user-profile-add-phone_number" data-method="POST" data-path="api/v1/user/profile/add-phone_number" data-authed="1" data-hasfiles="0" data-headers='{"Content-Type":"application\/json","Accept":"application\/json"}' onsubmit="event.preventDefault(); executeTryOut('POSTapi-v1-user-profile-add-phone_number', this);">
+<h3>
+    Request&nbsp;&nbsp;&nbsp;
+        <button type="button" style="background-color: #8fbcd4; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-tryout-POSTapi-v1-user-profile-add-phone_number" onclick="tryItOut('POSTapi-v1-user-profile-add-phone_number');">Try it out ⚡</button>
+    <button type="button" style="background-color: #c97a7e; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-canceltryout-POSTapi-v1-user-profile-add-phone_number" onclick="cancelTryOut('POSTapi-v1-user-profile-add-phone_number');" hidden>Cancel</button>&nbsp;&nbsp;
+    <button type="submit" style="background-color: #6ac174; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-executetryout-POSTapi-v1-user-profile-add-phone_number" hidden>Send Request 💥</button>
+    </h3>
+<p>
+<small class="badge badge-black">POST</small>
+ <b><code>api/v1/user/profile/add-phone_number</code></b>
+</p>
+<p>
+<label id="auth-POSTapi-v1-user-profile-add-phone_number" hidden>Authorization header: <b><code>Bearer </code></b><input type="text" name="Authorization" data-prefix="Bearer " data-endpoint="POSTapi-v1-user-profile-add-phone_number" data-component="header"></label>
+</p>
+<h4 class="fancy-heading-panel"><b>Body Parameters</b></h4>
+<p>
+<b><code>phone_number</code></b>&nbsp;&nbsp;<small>string</small>  &nbsp;
+<input type="text" name="phone_number" data-endpoint="POSTapi-v1-user-profile-add-phone_number" data-component="body" required  hidden>
+<br>
+PhoneNumber.</p>
+
+</form>
+<h2>Verify Added Phone Number</h2>
+<p><small class="badge badge-darkred">requires authentication</small></p>
+<p>Verify passcode provided by the user on adding a new phone number.</p>
+<blockquote>
+<p>Example request:</p>
+</blockquote>
+<pre><code class="language-bash">curl -X POST \
+    "http://localhost/api/v1/user/profile/validate-phone_number" \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -d '{"phone_number":"et","passcode":"a"}'
+</code></pre>
+<pre><code class="language-javascript">const url = new URL(
+    "http://localhost/api/v1/user/profile/validate-phone_number"
+);
+
+let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+};
+
+let body = {
+    "phone_number": "et",
+    "passcode": "a"
+}
+
+fetch(url, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(body),
+}).then(response =&gt; response.json());</code></pre>
+<div id="execution-results-POSTapi-v1-user-profile-validate-phone_number" hidden>
+    <blockquote>Received response<span id="execution-response-status-POSTapi-v1-user-profile-validate-phone_number"></span>:</blockquote>
+    <pre class="json"><code id="execution-response-content-POSTapi-v1-user-profile-validate-phone_number"></code></pre>
+</div>
+<div id="execution-error-POSTapi-v1-user-profile-validate-phone_number" hidden>
+    <blockquote>Request failed with error:</blockquote>
+    <pre><code id="execution-error-message-POSTapi-v1-user-profile-validate-phone_number"></code></pre>
+</div>
+<form id="form-POSTapi-v1-user-profile-validate-phone_number" data-method="POST" data-path="api/v1/user/profile/validate-phone_number" data-authed="1" data-hasfiles="0" data-headers='{"Content-Type":"application\/json","Accept":"application\/json"}' onsubmit="event.preventDefault(); executeTryOut('POSTapi-v1-user-profile-validate-phone_number', this);">
+<h3>
+    Request&nbsp;&nbsp;&nbsp;
+        <button type="button" style="background-color: #8fbcd4; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-tryout-POSTapi-v1-user-profile-validate-phone_number" onclick="tryItOut('POSTapi-v1-user-profile-validate-phone_number');">Try it out ⚡</button>
+    <button type="button" style="background-color: #c97a7e; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-canceltryout-POSTapi-v1-user-profile-validate-phone_number" onclick="cancelTryOut('POSTapi-v1-user-profile-validate-phone_number');" hidden>Cancel</button>&nbsp;&nbsp;
+    <button type="submit" style="background-color: #6ac174; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-executetryout-POSTapi-v1-user-profile-validate-phone_number" hidden>Send Request 💥</button>
+    </h3>
+<p>
+<small class="badge badge-black">POST</small>
+ <b><code>api/v1/user/profile/validate-phone_number</code></b>
+</p>
+<p>
+<label id="auth-POSTapi-v1-user-profile-validate-phone_number" hidden>Authorization header: <b><code>Bearer </code></b><input type="text" name="Authorization" data-prefix="Bearer " data-endpoint="POSTapi-v1-user-profile-validate-phone_number" data-component="header"></label>
+</p>
+<h4 class="fancy-heading-panel"><b>Body Parameters</b></h4>
+<p>
+<b><code>phone_number</code></b>&nbsp;&nbsp;<small>string</small>  &nbsp;
+<input type="text" name="phone_number" data-endpoint="POSTapi-v1-user-profile-validate-phone_number" data-component="body" required  hidden>
+<br>
+Phone Number that received OTP.</p>
+<p>
+<b><code>passcode</code></b>&nbsp;&nbsp;<small>string</small>  &nbsp;
+<input type="text" name="passcode" data-endpoint="POSTapi-v1-user-profile-validate-phone_number" data-component="body" required  hidden>
+<br>
+Passcode that was sent via SMS.</p>
+
+</form>
+<h2>Set Default Phone Number</h2>
+<p><small class="badge badge-darkred">requires authentication</small></p>
+<p>Mark Registered PhoneNumber as Default.</p>
+<blockquote>
+<p>Example request:</p>
+</blockquote>
+<pre><code class="language-bash">curl -X POST \
+    "http://localhost/api/v1/user/profile/set-default-phone_number" \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -d '{"phone_number":"consequatur"}'
+</code></pre>
+<pre><code class="language-javascript">const url = new URL(
+    "http://localhost/api/v1/user/profile/set-default-phone_number"
+);
+
+let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+};
+
+let body = {
+    "phone_number": "consequatur"
+}
+
+fetch(url, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(body),
+}).then(response =&gt; response.json());</code></pre>
+<div id="execution-results-POSTapi-v1-user-profile-set-default-phone_number" hidden>
+    <blockquote>Received response<span id="execution-response-status-POSTapi-v1-user-profile-set-default-phone_number"></span>:</blockquote>
+    <pre class="json"><code id="execution-response-content-POSTapi-v1-user-profile-set-default-phone_number"></code></pre>
+</div>
+<div id="execution-error-POSTapi-v1-user-profile-set-default-phone_number" hidden>
+    <blockquote>Request failed with error:</blockquote>
+    <pre><code id="execution-error-message-POSTapi-v1-user-profile-set-default-phone_number"></code></pre>
+</div>
+<form id="form-POSTapi-v1-user-profile-set-default-phone_number" data-method="POST" data-path="api/v1/user/profile/set-default-phone_number" data-authed="1" data-hasfiles="0" data-headers='{"Content-Type":"application\/json","Accept":"application\/json"}' onsubmit="event.preventDefault(); executeTryOut('POSTapi-v1-user-profile-set-default-phone_number', this);">
+<h3>
+    Request&nbsp;&nbsp;&nbsp;
+        <button type="button" style="background-color: #8fbcd4; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-tryout-POSTapi-v1-user-profile-set-default-phone_number" onclick="tryItOut('POSTapi-v1-user-profile-set-default-phone_number');">Try it out ⚡</button>
+    <button type="button" style="background-color: #c97a7e; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-canceltryout-POSTapi-v1-user-profile-set-default-phone_number" onclick="cancelTryOut('POSTapi-v1-user-profile-set-default-phone_number');" hidden>Cancel</button>&nbsp;&nbsp;
+    <button type="submit" style="background-color: #6ac174; padding: 5px 10px; border-radius: 5px; border-width: thin;" id="btn-executetryout-POSTapi-v1-user-profile-set-default-phone_number" hidden>Send Request 💥</button>
+    </h3>
+<p>
+<small class="badge badge-black">POST</small>
+ <b><code>api/v1/user/profile/set-default-phone_number</code></b>
+</p>
+<p>
+<label id="auth-POSTapi-v1-user-profile-set-default-phone_number" hidden>Authorization header: <b><code>Bearer </code></b><input type="text" name="Authorization" data-prefix="Bearer " data-endpoint="POSTapi-v1-user-profile-set-default-phone_number" data-component="header"></label>
+</p>
+<h4 class="fancy-heading-panel"><b>Body Parameters</b></h4>
+<p>
+<b><code>phone_number</code></b>&nbsp;&nbsp;<small>string</small>  &nbsp;
+<input type="text" name="phone_number" data-endpoint="POSTapi-v1-user-profile-set-default-phone_number" data-component="body" required  hidden>
+<br>
+Phone Number that received OTP.</p>
+
 </form>
     </div>
     <div class="dark-box">
