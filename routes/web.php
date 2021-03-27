db577cca77d77268f22fad6e31d0bf1d9a9b1078
Kevin Mukhami <mukhami@deveint.com>
2021-03-27 21:24:53 +0300

    moov

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('/moov', function () {
     ));
     $response = curl_exec($curl);
     curl_close($curl);
-    echo $response;
+    dd($response);
 });
 Route::get('/', function () {
     return \Illuminate\Support\Facades\Redirect::to('admin/login');
