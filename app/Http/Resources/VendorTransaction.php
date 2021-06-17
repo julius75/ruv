443f786e53f421ad18fb3cff8203d58d994af131
Kevin Mukhami <mukhami@deveint.com>
2021-06-17 15:57:57 +0300

    fixed listing vendor transactions

--- app/Http/Resources/VendorTransaction.php
+++ app/Http/Resources/VendorTransaction.php
@@ -21,33 +21,47 @@ class VendorTransaction extends JsonResource
      */
     public function toArray($request)
     {
-        if ($this->transactionable_type == OrangeAirtimeTransaction::class){
-            return [
-                'id' => $this->id,
-                'reference_number' => $this->reference_number,
-                'amount' => $this->amount,
-                'phone_number' => $this->transactionable->customer_msisdn,
-                'issued' => $this->transactionable->issued,
-                'status' => $this->status,
-                'user' => User::find($this->user_id,['id', 'first_name', 'last_name']),
-                'provider' => Provider::find(1,['id', 'name', 'logo']),
-                'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:i'),
-            ];
-        }elseif ($this->transactionable_type == MoovMoneyTransaction::class){
-            return [
-                'id' => $this->id,
-                'reference_number' => $this->reference_number,
-                'amount' => $this->amount,
-                'phone_number' => $this->transactionable->customer_msisdn,
-                'issued' => $this->transactionable->issued,
-                'status' => $this->status,
-                'user' => User::find($this->user_id,['id', 'first_name', 'last_name']),
-                'provider' => Provider::find(2,['id', 'name', 'logo']),
-                'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:i'),
-            ];
-        }
-        else{
-            return [];
-        }
+        return [
+            'id' => $this->id,
+            'reference_number' => $this->reference_number,
+            'amount' => $this->amount,
+            'phone_number' => $this->transactionable->customer_msisdn,
+            'issued' => $this->transactionable->issued,
+            'status' => $this->status,
+            'user' => User::find($this->user_id,['id', 'first_name', 'last_name']),
+            'provider' => Provider::find($this->transactionable->provider_id,['id', 'name', 'logo']),
+            'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:i'),
+        ];
+
+
+
+//        if ($this->transactionable_type == OrangeAirtimeTransaction::class){
+//            return [
+//                'id' => $this->id,
+//                'reference_number' => $this->reference_number,
+//                'amount' => $this->amount,
+//                'phone_number' => $this->transactionable->customer_msisdn,
+//                'issued' => $this->transactionable->issued,
+//                'status' => $this->status,
+//                'user' => User::find($this->user_id,['id', 'first_name', 'last_name']),
+//                'provider' => Provider::find($this->transactionable->provider_id,['id', 'name', 'logo']),
+//                'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:i'),
+//            ];
+//        }elseif ($this->transactionable_type == MoovMoneyTransaction::class){
+//            return [
+//                'id' => $this->id,
+//                'reference_number' => $this->reference_number,
+//                'amount' => $this->amount,
+//                'phone_number' => $this->transactionable->customer_msisdn,
+//                'issued' => $this->transactionable->issued,
+//                'status' => $this->status,
+//                'user' => User::find($this->user_id,['id', 'first_name', 'last_name']),
+//                'provider' => Provider::find($this->transactionable->provider_id,['id', 'name', 'logo']),
+//                'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:i'),
+//            ];
+//        }
+//        else{
+//            return [];
+//        }
     }
 }
